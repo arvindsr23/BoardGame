25208855c51e5e4392d89c28d810f56ab7921293
Arvind <arvindsr23@gmail.com>
2017-03-03 22:52:54 -0500

    Initial List

---
+++ NumTuple.py
@@ -0,0 +1,10 @@
+from collections import namedtuple
+
+player = namedtuple('player','name score count')
+
+Earth = player(name = 'Earth', score = '104',count = '14')
+#Mars =  player('Mars','105','6')
+
+print(Earth.name,Earth.score,Earth.count)
+
+
